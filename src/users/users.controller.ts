a2709e5dbd193362858f938cf956654e61c801d3
Raúl A. H. Silva <105474215+ShadowCoderXR@users.noreply.github.com>
2023-10-02 09:07:41 -0600

    feat(users): implement CRUD for users in NestJS

---
+++ src/users/users.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Post, Get, Patch, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+    constructor(private usersService: UsersService) {}
+
+    @Get()
+    getUsers(): Promise<User[]> {
+        return this.usersService.getUsers();
+    }
+
+    @Post()
+    createUser(@Body() newUser: CreateUserDto){
+        return this.usersService.createUser(newUser);
+    }
+
+    @Get(':id')
+    getUserById( @Param('id', ParseIntPipe) id: number){
+        return this.usersService.getUserById(id);
+    }
+
+    @Patch(':id')
+    updateUser(@Param('id', ParseIntPipe) id: number, @Body() user: UpdateUserDto){
+        return this.usersService.updateUser(id, user);
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id', ParseIntPipe) id: number){
+        return this.usersService.deleteUser(id);
+    }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,71 @@
+import { Injectable, NotFoundException, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { HttpException } from '@nestjs/common/exceptions';
+
+@Injectable()
+export class UsersService {
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+
+    async createUser(user: CreateUserDto){
+        const userFound = await this.userRepository.findOne({
+            where: {
+                username: user.username
+            }
+        });
+
+        if(userFound){
+            throw new ConflictException('Username already exists');
+        }
+
+        const newUser = this.userRepository.create(user);
+        return this.userRepository.save(newUser);
+    }
+
+    getUsers(){
+        return this.userRepository.find();
+    }
+
+    async getUserById(id: number){
+        const userFound = await this.userRepository.findOne({
+            where: {
+                id
+            }
+        });
+
+        if(!userFound){
+            throw new NotFoundException('User not found');
+        }
+
+        return userFound;
+    }
+
+    async updateUser(id: number, user: UpdateUserDto){
+        const userFound = await this.userRepository.findOne({
+            where: {
+                id
+            }
+        });
+
+        if(!userFound){
+            throw new NotFoundException('User not found');
+        }
+
+        this.userRepository.update(id, user);
+
+        return {'message': 'User updated successfully'}
+    }
+
+    async deleteUser(id: number){
+        const result = await this.userRepository.delete(id);
+
+        if(result.affected === 0){
+            throw new NotFoundException('User not found');
+        }
+
+        return {'message': 'User deleted successfully'}
+    }
+}
